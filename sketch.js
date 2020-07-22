f9206db3759d6537340ffdd533f0942077d8f5a8
shamita1512 <chemica1cooki3@gmail.com>
2020-07-22 17:54:30 +0530

    Added image

--- sketch.js
+++ sketch.js
@@ -9,7 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1_img, car2_img, car3_img, car4_img;
+var trackImg;
 
+ function preload(){
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  trackImg = loadImage("images/track.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -21,6 +30,7 @@ function setup(){
 
 
 function draw(){
+  
   if(playerCount === 4){
     game.update(1);
   }
