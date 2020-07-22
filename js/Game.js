bd1c0f435e40783ec665581d905db240a281de1a
shamita1512 <chemica1cooki3@gmail.com>
2020-07-22 17:54:03 +0530

    Added car image

--- js/Game.js
+++ js/Game.js
@@ -30,17 +30,21 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1", car1_img);
     car2 = createSprite(300,200);
+    car2.addImage("car2", car2_img);
     car3 = createSprite(500,200);
+    car3.addImage("car3", car3_img);
     car4 = createSprite(700,200);
+    car4.addImage("car4", car4_img);
     cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-
+    background("#70543E");
     Player.getPlayerInfo();
-    
+    image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5);
     if(allPlayers !== undefined){
       //var display_position = 100;
       
@@ -48,7 +52,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 150;
       var y;
 
       for(var plr in allPlayers){
@@ -59,8 +63,8 @@ class Game {
         x = x + 200;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
+        cars[index-1].position.x = x;
+        cars[index-1].position.y = y;
 
         if (index === player.index){
           cars[index - 1].shapeColor = "red";
@@ -78,7 +82,13 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+    if(player.distance>3700){
+      gameState = 2;
+    }
     drawSprites();
+  }
+  end(){
+    console.log("game ended");
+    game.update(2);
   }
 }
